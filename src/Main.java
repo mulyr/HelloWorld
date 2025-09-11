eaa3b866291995a4d90e6299dc69fb9e097ef475
MULYRUFMAN <mulyr@nycstudents.net>
2025-09-11 15:17:10 -0400

    Added user input

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("What's your name?");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        System.out.println("Hello, " + name + "!");
 
     }
 }
